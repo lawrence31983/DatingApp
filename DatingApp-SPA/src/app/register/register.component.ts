80f781302ee62d9c7e3762ca6f2ca61d7d880043
lawrence31983 <lawrence31983@gmail.com>
2019-02-27 23:04:00 -0700

    starting reactive register form

--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { FormGroup, FormControl } from '@angular/forms';
+import { formControlBinding } from '@angular/forms/src/directives/reactive_directives/form_control_directive';
 
 
 @Component({
@@ -8,22 +10,31 @@ import { AlertifyService } from '../_services/alertify.service';
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
+
 export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
+  registerForm: FormGroup;
+
   constructor(private authservice: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
+    this.registerForm =  new FormGroup({
+      username: new FormControl(),
+      password: new FormControl(),
+      confirmPassword: new FormControl()
+    });
   }
 
   register() {
-    this.authservice.register(this.model).subscribe(() => {
-      console.log('Registration successful');
-      this.alertify.success('Registration Successful');
-    }, error => {
-      console.log(error);
-      this.alertify.error(error);
-    });
+    // this.authservice.register(this.model).subscribe(() => {
+    //   console.log('Registration successful');
+    //   this.alertify.success('Registration Successful');
+    // }, error => {
+    //   console.log(error);
+    //   this.alertify.error(error);
+    // });
+    console.log(this.registerForm.value);
   }
 
   cancel() {
